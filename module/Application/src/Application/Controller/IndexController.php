ecf75115c124ec3c5c3f935d309ee2ad29955757
Raven Sampson <raven3419@gmail.com>
2017-11-16 15:32:06 -0500

    fixing footer bug

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -533,7 +533,7 @@ class IndexController extends AbstractActionController
     {
         $years = $this->lundProductService->getPartService()->getVehYear();
         $number ="";
-        for($x=0; $x<30; $x++) {
+        for($x=0; $x<40; $x++) {
             $number .= rand(0, 57).", ";
         }
         
